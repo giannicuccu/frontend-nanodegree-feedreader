141bda4d2c371c928ea38afadbfa05a276145b0e
Gianni Cuccu <gianni.cuccu@gmail.com>
2018-05-25 10:45:07 +0200

    REFACTOR all specs

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,7 +8,7 @@
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 2500; // 
+//jasmine.DEFAULT_TIMEOUT_INTERVAL = 2500; // 
 
 
 $(function() {
@@ -16,8 +16,7 @@ $(function() {
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
-
-    
++
    describe('RSS Feeds', () => {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
@@ -36,22 +35,11 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it('Has valid URL (defined and not empty)', () => {
-
-            //##### URL VALIDATION REGEX PATTERN FROM:
-            //https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
-            const urlPattern =  new RegExp('^(https?:\\/\\/)?'+ // protocol
-            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name and extension
-            '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-            '(\\:\\d+)?'+ // port
-            '(\\/[-a-z\\d%@_.~+&:]*)*'+ // path
-            '(\\?[;&a-z\\d%@_.,~+&:=-]*)?'+ // query string
-            '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-            //##### 
-
-            const filterURLS = allFeeds => allFeeds.filter(feed => !urlPattern.test(feed.url));
-            const invalidURLS = filterURLS(allFeeds);
-            expect(invalidURLS.length).toBe(0);
+        it('Has valid URL', () => {
+            allFeeds.forEach(function(feed){
+                expect(feed.url).toBeDefined();
+                expect((feed.url.length)).toBeGreaterThan(0);                
+                });
         });
 
 
@@ -59,12 +47,11 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-
-        it('Has valid Name (defined and not empty, max 48 chars)', () => {
-            const namePattern =  new RegExp('^.{1,48}$'); 
-            const filterNames = allFeeds => allFeeds.filter(feed => !namePattern.test(feed.name.trim()));
-            const invalidNames = filterNames(allFeeds);
-            expect(invalidNames.length).toBe(0);
+        it('Has valid Name', () => {
+            allFeeds.forEach(function(feed){
+                expect(feed.name).toBeDefined();
+                expect((feed.name).length).toBeGreaterThan(0);
+                });
         });
     });
 
@@ -79,14 +66,14 @@ $(function() {
          */
         it('Menu is hidden by default', () => { 
             // check for .menu-hidden  class in body
-            const bodyClass = document.querySelector('body').classList.contains('menu-hidden');
-            expect(bodyClass).toBe(true);
+            expect($('body').hasClass('menu-hidden')).toBe(true);
             
-            // check offcanvas left position of menu
+            // ###### Additional check offcanvas left position of menu
             const menuElement = document.body.getElementsByClassName('slide-menu')[0];
             const bounding = menuElement.getBoundingClientRect();
             bounding.left === bounding.x && bounding.left + bounding.width === 0? leftPosCheck = true: leftPosCheck = false;
             expect(leftPosCheck).toBe(true);
+            // ######
         });
     
 
@@ -97,19 +84,15 @@ $(function() {
           */
 
         it('Menu visibility is toggled by icon click', (done) => {
-            // get the menu toggler 
-            const menuToggler = document.getElementsByClassName('menu-icon-link')[0];
 
             // emulate first click
-            menuToggler.click();
-            let bodyClass = document.querySelector('body').classList.contains('menu-hidden');
-            expect(bodyClass).toBe(false);
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
             
             // emulate second click after 600ms
             setTimeout(() => {
-                menuToggler.click();
-                bodyClass = document.querySelector('body').classList.contains('menu-hidden');
-                expect(bodyClass).toBe(true);
+                $('.menu-icon-link').click();
+                expect($('body').hasClass('menu-hidden')).toBe(true);
                 done();
             }, 600);
         });
@@ -124,25 +107,16 @@ $(function() {
         * Remember, loadFeed() is asynchronous so this test will require
         * the use of Jasmine's beforeEach and asynchronous done() function.
         */ 
-       
-       beforeEach(function(done){
 
+       beforeEach(function(done){
             //spyOn(window, 'loadFeed'); // TODO: how to spy loadFeed call???
             loadFeed(0, () => done());
-                
             });
-
 
         it('Load feed completed', (done) => {
-
-            let feedList = document.querySelectorAll('.feed .entry');
-            expect(feedList.length).toBeGreaterThan(0);
-            //expect(loadTest.loadfunc).toHaveBeenCalled();
+            expect($('.entry').length).toBeGreaterThan(0);
             done();
             });
-
-
-
     });
 
 
@@ -156,30 +130,20 @@ $(function() {
         */      
 
         beforeEach(function(done) {
-            
-            // get the .feed element innerHTML
-            this.InitialContent = document.querySelectorAll('.feed')[0].innerHTML;
-            
-            // get a random index different from 0
-            const randomFeedIndex = Math.floor(Math.random() * ((allFeeds.length-1) - 1 + 1) + 1);
-            
-            // load another random feed
-            loadFeed(randomFeedIndex, ()=>done());
-            
+            // Async load load first feed and then second feed
+            loadFeed(0, () => {
+                InitialContent = $('.feed').html();
+                loadFeed(1, () => {
+                    updatedContent = $('.feed').html();
+                    done();
+                });
             });
-
-            it('Change content', function(done) {
-                // get the updated .feed element innerHTML and check against the previous 
-                const updatedContent = document.querySelectorAll('.feed')[0].innerHTML;
-                expect(this.InitialContent).not.toEqual(updatedContent);
-                done();
-            });
-            
-            // reload the first feed after the test
-            afterEach(function(done){
-                loadFeed(0, ()=>done());
-                done();
-            });
-         
         });
+
+        it('Change content', function() {
+            expect(InitialContent).not.toEqual(updatedContent);
+        });
+        
+
+    });
 }());
